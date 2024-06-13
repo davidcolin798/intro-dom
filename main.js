a56cbf2e2c119c06cc9b085c3432bf6b4f04e535
dcolin798@gmail.com <dcolin798@gmail.com>
2024-06-13 17:48:47 -0600

    creando un listador

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// Add JS here
+const todos =[];
+window.onload=()=>{
+    const form = document.getElementById('todo-form');
+    form.onsubmit = (e)=>{
+        e.preventDefault();
+        const todo =document.getElementById('todo');
+        const todoTexto = todo.value;
+        todo.value ='';
+        todos.push(todoTexto);
+        const todoList = document.getElementById('todo-list');
+        todoList.innerHTML='';
+        for (let i = 0; i < todos.length; i++){
+            todoList.innerHTML+= '<li>'+ todos[i] +'</li>';
+        }
+    }
+}
