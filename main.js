71ccc0a0e68e434f9ac0cb55a9ed2813042906f0
dcolin798@gmail.com <dcolin798@gmail.com>
2024-06-26 11:48:14 -0600

    cambios pequenos

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const todos =[];
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
 const render = () => {
     const todoList = document.getElementById('todo-list');       
     const todosTemplate = todos.map(t =>'<li>'+ t + '</li>');
@@ -9,11 +9,17 @@ const render = () => {
         elemento.addEventListener('click',()=>{
             elemento.parentNode.removeChild(elemento);
             todos.splice(i, 1);
+            actualizarTodos()
             render()
         })
     })
 }
+const actualizarTodos=(todos)=>{
+    const todoStrings = JSON.stringify(todos);
+    localStorage.setItem('todos',todoStrings);
+}
 window.onload=()=>{
+    render()
     const form = document.getElementById('todo-form');
     form.onsubmit = (e)=>{
         e.preventDefault();
@@ -21,6 +27,7 @@ window.onload=()=>{
         const todoTexto = todo.value;
         todo.value ='';
         todos.push(todoTexto);
+        actualizarTodos()
         render()
         
         
